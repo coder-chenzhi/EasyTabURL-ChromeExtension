5ac1535c917ba03be95036e9b1a25a2fdae1b88d
coder-chenzhi <zilingqishi@gmail.com>
2016-10-05 19:27:55 +0800

    add copyright

--- README.md
+++ README.md
@@ -1,10 +1,9 @@
 # TabManager-ChromeExtension
-Conveniently manage your opened tab
-modfied from repository [Get-opened-tabs-URLs-Chrome-extension](https://github.com/cbenz/Get-opened-tabs-URLs-Chrome-extension)
+Conveniently manage your opened tab 
 
 ## Features
-- get all opened tabs' urls 
-- copy all opened tabs' urls to clipboard(Chrome only!)
-- save all opened tabs' urls to local file
+- get all tabs' urls
+- copy all tabs' urls to clipboard(Chrome only!)
+- save all tabs' urls to local file
 - read local file and open all urls in the file(file generated from 'save' function)
 
--- TabManager.js
+++ TabManager.js
@@ -1,26 +1,12 @@
-/*
-<license>
-Get opened tabs URLs - a Google Chrome extension
-Copyright 2010 Christophe Benz.
+/*LICENSE
+Copyright (c) 2016 Coder-chenzhi
 
-This program is free software; you can redistribute it and/or
-modify it under the terms of the GNU General Public License
-as published by the Free Software Foundation; either version 2
-of the License, or (at your option) any later version.
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
 
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
-</license>
-
-Icon from Silk icon set:
-http://www.famfamfam.com/lab/icons/silk/
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
-
 function list(tabs) {
   var contents = '';
   for (var i = 0; i < tabs.length; i++) {
